fcf1948a1e7581e3cdbbd74c50b7adbd5cde6470
Nico DeMilio <njdemilio@gmail.com>
2025-01-18 11:23:42 -0500

    fix bulma typo for deployment

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'Bulma/css/bulma.css'
+import 'bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const app = createApp(App)
